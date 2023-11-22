a8721377355e8f47cb666212bc62164ec1887a53
Polam <S562045@nwmissouri.edu>
2023-11-22 15:27:44 -0600

    update the record

--- routes/eagle.js
+++ routes/eagle.js
@@ -1,10 +1,27 @@
 var express = require('express');
 const eagle_controlers= require('../controllers/eagle');
 var router = express.Router();
-/* GET eagles */
+
+//new code for Assignment13
+//A little function to check if we have an authorized user and continue on or
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
+  
+/* GET costumes */
 router.get('/', eagle_controlers.eagle_view_all_Page );
-router.get('/detail', eagle_controlers.eagle_view_one_Page);
-router.get('/create', eagle_controlers.eagle_create_Page);
-router.get('/update', eagle_controlers.eagle_update_Page);
-router.get('/delete', eagle_controlers.eagle_delete_Page);
+router.get('/eagle/:id', eagle_controlers.eagle_detail); 
+/* GET detail cars page */ 
+router.get('/detail',secured, eagle_controlers.eagle_view_one_Page); 
+/* GET create cars page */ 
+router.get('/create',secured, eagle_controlers.eagle_create_Page);
+/* GET create update page */
+router.get('/update',secured, eagle_controlers.eagle_update_Page);
+/* GET delete costume page */
+router.get('/delete',secured, eagle_controlers.eagle_delete_Page);
 module.exports = router;
